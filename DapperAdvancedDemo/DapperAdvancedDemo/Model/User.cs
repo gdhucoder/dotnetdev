f495214d692576532eaec0fea27f77ebb961b2a2
gdhucoder <gdong.hu@gmail.com>
2021-04-18 22:48:53 +0800

    Adavanced dapper user example

---
+++ DapperAdvancedDemo/DapperAdvancedDemo/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperAdvancedDemo.Model
+{
+    public class User
+    {
+        public string UserName { get; set; }
+    }
+
+    public class PagedResults<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int Total { get; set; }
+    }
+}
---
+++ DapperAdvancedDemo/DapperAdvancedDemo/Service/UserService.cs
@@ -0,0 +1,40 @@
+﻿using DapperAdvancedDemo.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DapperAdvancedDemo.Service
+{
+    public class UserService
+    {
+        public UserService()
+        {
+        }
+        public PagedResults<User> FindAllUsers()
+        {
+            var result = new PagedResults<User>();
+            using (IDbConnection cnn = new MySqlConnection("Server=127.0.0.1;Port=5004;Uid=root;Password=password;Database=testdb"))
+            {
+                string sql = "select * from user where id = '1'; select count(*) from user;";
+                
+                using (var mult = cnn.QueryMultiple(sql))
+                {
+                    // user list
+                    result.Items = mult.Read<User>().ToList();
+                    // total records
+                    result.Total = mult.ReadFirst<int>();
+                }
+
+                foreach (var user in result.Items)
+                {
+                    Console.WriteLine($"{ user.UserName }");
+                }
+            }
+            return result;
+        }
+    }
+}
