7c0294bbd99ea5e2416439a6a72c730d23edf975
gdhucoder <gdong.hu@gmail.com>
2021-07-23 23:00:27 +0800

    file base64 demo

---
+++ filedemo/filetobase64/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace filetobase64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                test();
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                Console.WriteLine($"{i} Time spend {elapsedMs}");
+            }
+
+        }
+
+
+        /// <summary>
+        /// 1. read file to byte[]
+        /// 2. convert byte[] to base64string
+        /// 3. sender: send base64string file content
+        /// 4. receiver: convert base64string back to original file
+        /// (each character in base64 string takes up 8 bits, represents 6 bits in byte array,
+        ///  so the size will roughly 1.33 times larger than the original file)
+        /// </summary>
+        public static void test()
+        {
+            byte[] oribyts = File.ReadAllBytes("E:/dotnetdev/filedemo/filetobase64/华为技术有限公司.pdf");
+            var file2base64string = Convert.ToBase64String(oribyts);
+            byte[] bytesfile = Convert.FromBase64String(file2base64string);
+            File.WriteAllBytes("E:/dotnetdev/filedemo/filetobase64/华为技术有限公司-convert.pdf", bytesfile);
+        }
+    }
+
+
+}
